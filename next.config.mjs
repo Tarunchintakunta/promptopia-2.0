02c5741ab510bc587417771734fbe34f19df6517
Tarun chintakunta <tarunchintakunta@gmail.com>
2025-02-14 06:00:23 +0530

    next config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
-    }
-  };
-  
+  reactStrictMode: true, // Recommended for debugging
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"], // Ensure mongoose is installed
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"], // Allowed image sources
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true, // Enable top-level await
+    };
+    return config;
+  },
+};
+
 export default nextConfig;
